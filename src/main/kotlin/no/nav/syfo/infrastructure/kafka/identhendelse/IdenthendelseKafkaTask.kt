3ac3a624c693f6c7f51e24396290206fd06db592
Anders Rognstad <anders.rognstad@nav.no>
2025-02-25 09:13:21 +0100

    Use max poll records 1000

--- src/main/kotlin/no/nav/syfo/infrastructure/kafka/identhendelse/IdenthendelseKafkaTask.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/kafka/identhendelse/IdenthendelseKafkaTask.kt
@@ -6,7 +6,6 @@ import no.nav.syfo.ApplicationState
 import no.nav.syfo.infrastructure.kafka.KafkaEnvironment
 import no.nav.syfo.infrastructure.kafka.kafkaAivenConsumerConfig
 import no.nav.syfo.infrastructure.kafka.launchKafkaConsumer
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import java.util.*
 
 const val PDL_AKTOR_TOPIC = "pdl.aktor-v2"
@@ -18,8 +17,6 @@ fun launchIdenthendelseConsumer(
 ) {
     val consumerProperties = Properties().apply {
         putAll(kafkaAivenConsumerConfig<KafkaAvroDeserializer>(kafkaEnvironment))
-        this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1"
-
         this[KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = kafkaEnvironment.aivenSchemaRegistryUrl
         this[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = false
         this[KafkaAvroDeserializerConfig.USER_INFO_CONFIG] =

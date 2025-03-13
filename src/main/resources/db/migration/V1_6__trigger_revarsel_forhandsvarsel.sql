9270d791cd52822d70b7b99f0f87bb1db65dcac5
geir-waagboe <geir.arne.waagbo@nav.no>
2025-03-13 16:26:37 +0100

    IS-3178: Trigger revarsling forhandsvarsel

--- src/main/resources/db/migration/V1_6__trigger_revarsel_forhandsvarsel.sql
+++ src/main/resources/db/migration/V1_6__trigger_revarsel_forhandsvarsel.sql
@@ -6,6 +6,6 @@ WHERE uuid in (
         v.published_at >= '2025-03-10T00:00:00' AND v.published_at <= '2025-03-12T10:04:00'
         AND NOT EXISTS (
             SELECT 1 FROM vurdering vu2
-            WHERE (vu2.created_at > vu.created_at AND vu2.personident = vu.personident
+            WHERE vu2.created_at > vu.created_at AND vu2.personident = vu.personident
         )
 );
